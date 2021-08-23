1bdaa01c93f0797575f31ef9ddf2f38494b7e4cd
Eddy Rufino <eddyjaair@gmail.com>
2021-08-23 17:46:00 -0500

    first step

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'roles' => \App\Http\Middleware\CheckRoles::class, // Role agregado por mi
+        'roles' => \App\Http\Middleware\CheckRoles::class, // Role agregado por mi.
     ];
 }
